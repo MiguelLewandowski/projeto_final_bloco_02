211759b44bdb6b3938aff33f319e02f2f70c7921
Miguel Lewandowski <miguelstrapazonl@gmail.com>
2025-12-17 14:57:02 -0300

    feat: adiciona documentação da api abrindo o projeto no navegador com a rota /swagger

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const config = new DocumentBuilder()
+    .setTitle('Farmácia API')
+    .setDescription('Projeto Integrador Farmácia - Bloco 02')
+    .setContact("Seu Nome", "https://github.com/seuusuario", "seuemail@email.com")
+    .setVersion('1.0')
+    .addTag('categorias')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
+
   process.env.TZ = '-03:00';
-
   app.useGlobalPipes(new ValidationPipe());
-
   app.enableCors();
 
   await app.listen(process.env.PORT ?? 4000);
