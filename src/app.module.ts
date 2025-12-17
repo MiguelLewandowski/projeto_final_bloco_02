860dd65bfc0f9197efd5076d3bfdafccbe5ae3d4
Miguel Lewandowski <miguelstrapazonl@gmail.com>
2025-12-17 14:51:57 -0300

    feat: adiciona o service e controller nos modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Categoria } from './categorias/entities/categoria.entity';
+import { CategoriaModule } from './categorias/categoria.module';
 
 
 @Module({
@@ -11,9 +13,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: 'root',
       password: 'root',
       database: 'db_farmacia',
-      entities: [],
+      entities: [Categoria],
       synchronize: true,
     }),  
+    CategoriaModule,
   ],
   controllers: [],
   providers: [],
